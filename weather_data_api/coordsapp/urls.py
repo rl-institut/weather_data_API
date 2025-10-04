2a619dd0bb95d6e014c3422d1d841b078cc992a8
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-10-04 21:24:51 +0200

    Add endpoint for OWEFE

--- weather_data_api/coordsapp/urls.py
+++ weather_data_api/coordsapp/urls.py
@@ -5,6 +5,7 @@ app_name = "coordsapp"
 
 urlpatterns = [
     path("", views.coordinates_form, name="coordinates_form"),
+    path("wefe/", views.wefe_data, name="wefe_data"),
     path("pps/", views.pps_weather_data, name="pps_weather_data"),
     path("download/", views.download_file, name="download_file"),
     path("get_csrf_token/", views.get_csrf_token, name="get_csrf_token"),
